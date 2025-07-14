7b38b990866c085560482606987e75151f1cabb7
Diwakar <the.walkersingh@hotmail.com>
2025-07-15 00:16:36 +0530

    Algo updated

--- JavaAlgorithm/src/main/java/com/thealgorithms/searches/HowManyTimesRotated.java
+++ JavaAlgorithm/src/main/java/com/thealgorithms/searches/HowManyTimesRotated.java
@@ -1,5 +1,4 @@
 package com.thealgorithms.searches;
-
 import java.util.Scanner;
 
 /*
@@ -26,38 +25,40 @@ import java.util.Scanner;
     2. [15,17,2,3,5] Number of rotations: 3
  */
 final class HowManyTimesRotated {
-    private HowManyTimesRotated() {
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-
-        System.out.println("The array has been rotated " + rotated(a) + " times");
-        sc.close();
-    }
-
-    public static int rotated(int[] a) {
-        int low = 0;
-        int high = a.length - 1;
-        int mid = 0; // low + (high-low)/2 = (low + high)/2
-
-        while (low <= high) {
-            mid = low + (high - low) / 2;
-
-            if (a[mid] < a[mid - 1] && a[mid] < a[mid + 1]) {
-                break;
-            } else if (a[mid] > a[mid - 1] && a[mid] < a[mid + 1]) {
-                high = mid + 1;
-            } else if (a[mid] > a[mid - 1] && a[mid] > a[mid + 1]) {
-                low = mid - 1;
-            }
-        }
-
-        return mid;
-    }
+   private HowManyTimesRotated() {
+   }
+   
+   public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  int[] a = new int[n];
+	  for(int i = 0; i < n; i++){
+		 a[i] = sc.nextInt();
+	  }
+	  
+	  System.out.println("The array has been rotated " + rotated(a) + " times");
+	  sc.close();
+   }
+   
+   public static int rotated(int[] a) {
+	  int low = 0;
+	  int high = a.length - 1;
+	  int mid = 0; // low + (high-low)/2 = (low + high)/2
+	  
+	  while(low <= high){
+		 mid = low + (high - low)/2;
+		 
+		 if(a[mid] < a[mid - 1] && a[mid] < a[mid + 1]){
+			break;
+		 }
+		 else if(a[mid] > a[mid - 1] && a[mid] < a[mid + 1]){
+			high = mid + 1;
+		 }
+		 else if(a[mid] > a[mid - 1] && a[mid] > a[mid + 1]){
+			low = mid - 1;
+		 }
+	  }
+	  
+	  return mid;
+   }
 }
